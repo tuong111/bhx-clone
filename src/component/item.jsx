e3aec625ec14cb6a5fd97abb328b2ff721f13d27
TuongNT <tuongtf@gmail.com>
2021-08-08 09:18:54 +0700

    modify item

--- src/component/item.jsx
+++ src/component/item.jsx
@@ -10,19 +10,16 @@ class Item extends React.Component {
   getProductID = (id) => {
     this.props.sendIDtoMain(id)
   }
-  getCancelProductID = (idCancel) => {
-    this.props.sendIDCancelToMain(idCancel)
-  }
   render() {
-    const { name, price, url , id, isSelected, idCancel} = this.props;
+    const { name, price, url , id, isSelected} = this.props;
     return (
         <div className="item">
             <img src={url} alt="" />
           <div className="product-detail">
             <p className="produc-name">{name}</p>
             <p>{price}</p>
-            <button className={!isSelected ? 'btnBuy' : 'btnBuy-active'} onClick = {()=> this.getProductID(id)}>Buy</button> <br/>
-            <button onClick = {()=> this.getCancelProductID(idCancel)}>Cancel Buy</button>
+            <button className={!isSelected ? 'btnBuy' : 'btnBuy-active'} 
+            onClick = {()=> this.getProductID(id)}>Buy</button> <br/>
           </div>
         </div>
     );
